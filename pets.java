b4e84664d33936a98cb5123f2c3501f32cd36b8a
shairston1 <71654888+shairston1@users.noreply.github.com>
2020-10-05 12:51:12 -0400

    Add files via upload

---
+++ pets.java
@@ -0,0 +1,35 @@
+package pets;
+
+public class pets {
+	private int id;
+	private String breed;
+	private String name;
+	private boolean serviceAnimal;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBreed() {
+		return breed;
+	}
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isServiceAnimal() {
+		return serviceAnimal;
+	}
+	public void setServiceAnimal(boolean serviceAnimal) {
+		this.serviceAnimal = serviceAnimal;
+	}
+	
+	
+}
